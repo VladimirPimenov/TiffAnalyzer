244a4ecff5e56985265c7c41c8b6e74a4fffdc7d
Vladimir Pimenov <pimenovv2000@gmail.com>
2026-01-15 17:35:39 +0300

    Добавлен тип оповещения при логгировании

--- src/Utils/logger.cpp
+++ src/Utils/logger.cpp
@@ -9,6 +9,23 @@ void Logger::loggingMesssageHandler(QtMsgType type, const QMessageLogContext &co
 	logFile->open(QFile::Append | QFile::Text);
 	
     out << QDateTime::currentDateTime().toString("dd-mm-yyyy hh:mm:ss ");
+    
+	switch(type)
+	{
+	    case QtInfoMsg:
+            out << "[INF] ";
+            break;
+        case QtWarningMsg:
+            out << "[WAR] ";
+            break;
+        case QtDebugMsg:
+            out << "[DEB] ";
+            break;
+        default:
+            out << "[ERR] ";
+            break;
+	}
+	
     out << msg << Qt::endl;
     
     logFile->close();
